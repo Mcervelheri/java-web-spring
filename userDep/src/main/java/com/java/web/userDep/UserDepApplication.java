811ae8a7ccc9ee6395ce683219f1cbca4d4299b1
Matheus Vinicius Cervelheri <82064152+Mcervelheri@users.noreply.github.com>
2022-10-29 15:01:54 -0300

    Changes to be committed:

---
+++ userDep/src/main/java/com/java/web/userDep/UserDepApplication.java
@@ -0,0 +1,13 @@
+package com.java.web.userDep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDepApplication.class, args);
+	}
+
+}
---
+++ userDep/src/test/java/com/java/web/userDep/UserDepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.web.userDep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
