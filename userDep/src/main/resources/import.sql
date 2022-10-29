bc891504f296f943821a79f08a168f6bc6837f72
Matheus Vinicius Cervelheri <82064152+Mcervelheri@users.noreply.github.com>
2022-10-29 17:13:54 -0300

    Changes to be committed: 	modified:   userDep/src/main/resources/import.sql

--- userDep/src/main/resources/import.sql
+++ userDep/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_department(name) VALUES ('Gest√£o');
-INSERT INTO tb_department(name) VALUES ('Inform√°tica');
+INSERT INTO tb_department(name) VALUES ('Gest„o');
+INSERT INTO tb_department(name) VALUES ('Inform·tica');
 
 INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Maria', 'maria@gmail.com');
 INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
